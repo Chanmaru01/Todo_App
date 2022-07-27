0d308395035bfdfad98d4663d9958d45c5466796
Chanmaru01 <chanmaru1336@gmail.com>
2022-07-28 07:20:49 +0900

    finish 2-8

--- db/migrate/20220725043617_create_todos.rb
+++ db/migrate/20220725043617_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
       t.string :content
-      t.references :goal, null: false, foreign_key: true
+      t.references :goal, foreign_key: true
       t.integer :position
       t.boolean :done, null: false, default: false
 
