8e6f90f2cd6c7348b183b2b77ad1c72b20fefecc
Chanmaru01 <chanmaru1336@gmail.com>
2022-07-28 06:35:33 +0900

    finish 2-7

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,57 +1,59 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :edit, :update, :destroy]
-
-  # GET /todos
-  def index
-    @todos = Todo.all
-  end
-
-  # GET /todos/1
-  def show
-  end
-
+# before_actionで,アクション前に実行されるメソッドを定義しています。
+before_action :autohenticate_user!
+before_action :set_goal
+before_action :set_todo, only: [:show, :edit, :update, :destroy, :sort]
+# current_userは、サインインしているuserのこと
   # GET /todos/new
   def new
-    @todo = Todo.new
+  #@goalに紐付いたtodosをnewしています
+    @todo = @goal.todo.new
   end
 
   # GET /todos/1/edit
   def edit
   end
 
+  def sort
+  end
+
   # POST /todos
   def create
-    @todo = Todo.new(todo_params)
+    @todo = @goals.todo.new(todo_params)
 
     if @todo.save
-      redirect_to @todo, notice: 'Todo was successfully created.'
+#create, #update の結果を @status という変数に反映する。
+      @status = true
     else
-      render :new
+      @status = false
     end
   end
 
   # PATCH/PUT /todos/1
   def update
+  # todo_paramsはこのクラスのprivate以下で定義しています。
     if @todo.update(todo_params)
-      redirect_to @todo, notice: 'Todo was successfully updated.'
+      @status = true
     else
-      render :edit
+      @status = false
     end
   end
 
   # DELETE /todos/1
   def destroy
     @todo.destroy
-    redirect_to todos_url, notice: 'Todo was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_todo
-      @todo = Todo.find(params[:id])
+    def set_goal
+      @goal = current_user.goals.find_by(id: params[:goal_id])
+      redirect_to(goal_url, alert: "ERROR!!") if @goal.blank?
     end
 
-    # Only allow a list of trusted parameters through.
+    def set_todo
+      @todo = @goal.todo.find_by(params[:id])
+    end
+
     def todo_params
       params.require(:todo).permit(:content, :goal_id, :position, :done)
     end
